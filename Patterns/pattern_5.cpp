58cd46913fa28d13b5c0088e18ed688546cc00e7
divyanshu2712 <divyanshuguptarmp790@gmail.com>
2024-08-04 14:17:53 +0530

    Patterns

---
+++ Patterns/pattern_5.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int count=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+            cout<<count<<" ";
+            count+=1;
+        }
+        cout<<endl;
+    }
+    return 0;
+}
